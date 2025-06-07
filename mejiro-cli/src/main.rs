b792547ee279a3e3c054c7b9ad24eb745c0ce769
Akira Noda <61897166+tsugumi-sys@users.noreply.github.com>
2025-06-07 14:48:58 +0900

    add entry point

--- mejiro-cli/src/main.rs
+++ mejiro-cli/src/main.rs
@@ -1,3 +1,45 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "mejiro-cli", version = env!("CARGO_PKG_VERSION"), about = "Blog management CLI")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Create a new blog post
+    New {
+        #[arg(short, long, default_value = "./posts")]
+        output: String,
+    },
+    /// Build HTML from published blog posts
+    Compile {
+        #[arg(short, long, default_value = "./posts")]
+        input: String,
+        #[arg(short, long, default_value = "./public")]
+        output: String,
+        #[arg(short, long, default_value = "./mejiro.yml")]
+        config_file: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::New { output } => {
+            println!("Creating a new post in: {}", output);
+        }
+        Commands::Compile {
+            input,
+            output,
+            config_file,
+        } => {
+            println!("Building site from input: {}", input);
+            println!("Output will be in: {}", output);
+            println!("Using config file: {}", config_file);
+        }
+    }
 }
